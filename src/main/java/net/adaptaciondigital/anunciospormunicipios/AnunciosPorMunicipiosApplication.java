67632b96e1713b85cae4dc3fa60ced71abe25da3
JVHC <carrcelt@gmail.com>
2021-03-06 23:05:43 +0100

    Commit inicial

---
+++ src/main/java/net/adaptaciondigital/anunciospormunicipios/AnunciosPorMunicipiosApplication.java
@@ -0,0 +1,13 @@
+package net.adaptaciondigital.anunciospormunicipios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnunciosPorMunicipiosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnunciosPorMunicipiosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/adaptaciondigital/anunciospormunicipios/AnunciosPorMunicipiosApplicationTests.java
@@ -0,0 +1,13 @@
+package net.adaptaciondigital.anunciospormunicipios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnunciosPorMunicipiosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
